0503a072ab1d9bedac12632dc89aa82313ac81a8
Joaquim Costa <jscosta88@gmail.com>
2024-02-12 20:45:56 -0500

    add prop-types validator

--- 6-comps/src/App.js
+++ 6-comps/src/App.js
@@ -1,14 +1,28 @@
-import Button from "./Botton";
+import Button from "./Button";
 
 function App() {
   return (
     <div>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <div>
+        <Button primary secondary>
+          Hello
+        </Button>
+      </div>
+      <div>
+        <Button>World</Button>
+      </div>
+      <div>
+        <Button>Ok</Button>
+      </div>
+      <div>
+        <Button>Click Me!</Button>
+      </div>
+      <div>
+        <Button>Bye</Button>
+      </div>
+      <div>
+        <Button>Oops</Button>
+      </div>
     </div>
   );
 }
