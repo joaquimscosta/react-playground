86902f0e77c8ca2a79094802e6f0972f4f59756e
Joaquim Costa <jscosta88@gmail.com>
2024-05-20 21:51:28 -0400

    chore: Add fetchUsers and addUser thunks to store

--- 8-media/src/components/UsersList.js
+++ 8-media/src/components/UsersList.js
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, fetchUsers } from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
 function UsersList() {
-  return "Users List";
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setIsCreatingUserError] = useState(null);
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+  useEffect(() => {
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      // .then(() => {
+      //   setIsLoadingUsers(false);
+      // })
+      .catch((error) => {
+        setLoadingUsersError(error);
+      })
+      .finally(() => {
+        setIsLoadingUsers(false);
+      });
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((error) => setIsCreatingUserError(error))
+      .finally(() => setIsCreatingUser(false));
+  };
+
+  if (isLoadingUsers) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (loadingUsersError) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3"></div>
+      {isCreatingUser ? (
+        "Creating User..."
+      ) : (
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      )}
+      {creatingUserError && "Error Creating User"}
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
--- 8-media/src/store/slices/usersSlice.js
+++ 8-media/src/store/slices/usersSlice.js
@@ -1,10 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
+import { addUser } from "../thunks/addUser";
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // update state object to show data is loading
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // show request is finished
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      // show request failed
+      state.isLoading = false;
+      state.error = action.error;
+    });
+
+    builder.addCase(addUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+    builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
